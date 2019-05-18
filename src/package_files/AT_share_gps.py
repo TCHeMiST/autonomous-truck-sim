05a828c8f02c1d7d903e3ed3c5197b731aea53bf
kurshakuz <abilkasov@gmail.com>
2019-05-18 20:06:38 +0600

    Update gps data sharing

--- src/package_files/AT_share_gps.py
+++ src/package_files/AT_share_gps.py
@@ -1,66 +1,87 @@
 #!/usr/bin/env python
 
-import socket, pickle
+import socket
+import pickle
 import rospy
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import String
-from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatFix, Imu
 import math
+from tf.transformations import euler_from_quaternion
+
+# def callback(data, args):
+#     sock = args
+#     # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.longitude)
+
+#     print("heloo")
+#     c, addr = sock.accept()
+#     print 'Got connection from', addr
+#     print(str(data.longitude))
+
+#     c.send(str(data.longitude))
+#     # c.close()
+
+# class Location:
+#     def __init__(self):
+#         self.latitude = 0
+#         self.longitude = 0
+#         self.altitude = 0
+
 
 def main():
-    s = socket.socket()          
+    s = socket.socket()
     print "Socket successfully created"
     port = 12345
 
-    s.bind(('', port))         
-    print "socket binded to %s" %(port)
+    s.bind(('', port))
+    print "socket binded to %s" % (port)
 
-    s.listen(5)      
-    print "socket is listening"   
+    s.listen(5)
+    print "socket is listening"
 
     try:
-        rospy.init_node('share_gps')      
+        rospy.init_node('share_truck_data')
 
         rospy.wait_for_service('/autonomous_truck/gps/enable')
+        rospy.wait_for_service('/autonomous_truck/inertial_unit/enable')
         try:
             print("service catched")
             # rospy.Subscriber("/autonomous_truck/gps/values", NavSatFix, callback, (s))
             # variable = Location()
             while(True):
-                data = rospy.wait_for_message("/autonomous_truck/gps/values", NavSatFix, timeout=1)
-                c, addr = s.accept()      
-                print 'Got connection from', addr 
+                gps_data = rospy.wait_for_message(
+                    "/autonomous_truck/gps/values", NavSatFix, timeout=1)
+                orientation_data = rospy.wait_for_message(
+                    "/autonomous_truck/inertial_unit/roll_pitch_yaw", Imu, timeout=1)
 
-                # variable = Location()
-                # variable.latitude = data.latitude
-                # variable.longitude = data.longitude
-                # variable.altitude = data.altitude
+                c, addr = s.accept()
+                print 'Got connection from', addr
 
-                # variable = {
-                #     "latitude": data.latitude,
-                #     "longitude": data.longitude,
-                #     "altitude": data.altitude
-                # }
+                new_latitude = (gps_data.latitude+103)*0.0000142
+                new_longitude = (gps_data.longitude-229)*0.0000142
+                    # (111111*math.degrees((math.cos(gps_data.longitude))))
 
-                # data_string = pickle.dumps(variable)
-                new_latitude = data.latitude/111111
-                new_longitude = data.longitude/(111111*(math.cos(data.longitude)))
+                orientation_list = [orientation_data.orientation.x, orientation_data.orientation.y, orientation_data.orientation.z, orientation_data.orientation.w]
+                (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
 
-                data_string = (str(new_latitude) + " " + str(new_longitude))
+                data_string = (str(new_latitude) + " " + str(new_longitude) + " " + str(roll) + " " + str(
+                    pitch) + " " + str(yaw))
                 print(data_string)
-                
+
                 c.send(data_string)
 
             c.close()
 
         except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
-        
+            print "Service call failed: %s" % e
+
         # rospy.spin()
-        
+
     except KeyboardInterrupt:
         # s.close()
         rospy.signal_shutdown("KeyboardInterrupt")
         raise
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
--- src/package_files/server/server.py
+++ src/package_files/server/server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, request, abort
 from flask_cors import CORS
 import socket, pickle
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -18,3 +19,26 @@ def share_location():
     s.close()      
 
     return data
+
+@app.route('/sendlocation', methods=['POST']) 
+def send_data():
+    if not request.get_json():
+        abort(400)
+        
+    location = request.get_json()
+    print(location)
+    return json.dumps(location)
+
+
+@app.route('/startstop', methods=['POST']) 
+def send_startstop():
+    if not request.get_json():
+        abort(400)
+        
+    startstop = request.get_json()
+
+    if (startstop['Action'] == 1):
+        print("START!")
+    else:
+        print("STOP!")
+    return json.dumps(startstop)
